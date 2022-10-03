6146726ecea031eaca72e55bd15e34e220fed8da
Prithvi Vishak <prithvivishak@gmail.com>
2022-10-03 12:57:35 +0530

    Allow user to specify filename

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 A python-based tool to get code metrics.
 
 This project needs Python 3 and matplotlib.
+
+Find then number of lines in a file with
+
+```
+python3 main.py <filename>
+```
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-fh=open("README.md","r")
-x=len(fh.readlines())
+import sys
+
+if len(sys.argv) < 1: # Checks if filename is omitted
+    print("Must provide filename!")
+    exit() # Exits program
+
+filename = sys.argv[1] # First command line argument
+
+fh = open(filename, "r")
+x = len(fh.readlines())
 print(x)
 fh.close()
-
-
-
-
-
